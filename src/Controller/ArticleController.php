b39f6c735f4941c69cff54f7fe935f6a0e3e12b0
unknown <saswata@moviloglobal.com>
2018-12-15 03:08:12 +0530

    3:2 KnPMarkdownBundle and its Services.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use Michelf\MarkdownInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,19 +20,45 @@ class ArticleController extends AbstractController
 	/**
 	* @Route("/news/{slug}", name="article_show")
 	*/
-	public function show($slug)
-	{
+	public function show($slug, MarkdownInterface $markdown)
+	{		
+		#dd($slug, $this);
 		$comments = [
 			'I ate a normal rock once. It did not taste like bacon!',
 			'Woohoo! I\'m going on an all-asteroid diet!',
 			'I like bacon too! Buy some grom my site -> www.bakinsomebacon.com',
 		];
 
-		#dump($slug, $this);
+
+        $articleContent = <<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. 
+
+Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+turkey shank eu pork belly meatball non cupim.
+Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
+laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
+capicola biltong frankfurter boudin cupim officia. 
+
+Exercitation fugiat consectetur ham. Adipisicing
+picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+occaecat lorem meatball prosciutto quis strip steak.
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+mollit quis officia meatloaf tri-tip swine. 
+
+Cow ut reprehenderit, buffalo incididunt in filet mignon
+strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
+cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
+fugiat.
+EOF;
+
+		$articleContent = $markdown->transform($articleContent);
 		return $this->render('article/show.html.twig', [
 			'title' => ucwords(str_replace('-', ' ', $slug)),
 			'slug' => $slug,
 			'comments' => $comments,
+			'articleContent' => $articleContent,
 		]);
 	}
 
