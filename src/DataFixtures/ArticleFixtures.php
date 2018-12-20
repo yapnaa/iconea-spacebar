e528bac254c6fc98be568d7a4793d6248468a09a
unknown <saswata@moviloglobal.com>
2018-12-20 15:02:16 +0530

    4:15 Using Faker for Seeding Data

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -6,12 +6,27 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class ArticleFixtures extends BaseFixture
 {
+	private static $articleTitles = [
+        'Why Asteroids Taste Like Bacon',
+        'Life on Planet Mercury: Tan, Relaxing and Fabulous',
+        'Light Speed Travel: Fountain of Youth or Fallacy',
+    ];
+    private static $articleImages = [
+        'asteroid.jpeg',
+        'mercury.jpeg',
+        'lightspeed.png',
+    ];
+    private static $articleAuthors = [
+        'Mike Ferengi',
+        'Amy Oort',
+    ];
+    
     public function loadData(ObjectManager $manager)
     {
 
     	$this->createMany(Article::class, 10, function(Article $article, $count) {
-	        $article->setTitle('Why Asteroids Taste Like Bacon')
-	            ->setSlug('why-asteroids-taste-like-bacon-'.rand(100, 999))
+	        $article->setTitle($this->faker->randomElement(self::$articleTitles))
+	            ->setSlug($this->faker->slug)
 	            ->setContent(<<<EOF
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
 lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
@@ -30,15 +45,15 @@ fugiat.
 EOF
 	        );
 	        // publish most articles
-	        if (rand(1, 10) > 2) {
-	            $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
-	        }
-	        $article->setAuthor('Mike Ferengi')
-	            ->setHeartCount(rand(5, 100))
-	            ->setImageFilename('asteroid.jpeg')
+            if ($this->faker->boolean(70)) {
+                $article->setPublishedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
+            }
+	        $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
+	            ->setHeartCount($this->faker->numberBetween(5, 100))
+	            ->setImageFilename($this->faker->randomElement(self::$articleImages))
 	        ;
 	    });
-	    
+
         $manager->flush();
     }
 }
