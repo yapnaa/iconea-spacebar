2cd7a0a8f80329b6b632cb5007f5006c669c71d7
unknown <saswata@moviloglobal.com>
2018-12-20 11:55:11 +0530

    4:11 Query Logic Re-use & Shortcuts

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,7 +9,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
-use Doctrine\ORM\EntityManagerInterface;
 
 class ArticleController extends AbstractController
 {
@@ -46,18 +45,11 @@ class ArticleController extends AbstractController
 	/**
 	* @Route("/news/{slug}", name="article_show")
 	*/
-	public function show($slug, SlackClient $slack, EntityManagerInterface $em)
+	public function show(Article $article, SlackClient $slack)
 	{
-		if($slug === "khaaaaaan") {
+		if($article->getSlug() === "khaaaaaan") {
             $slack->sendMessage('Kahn', 'Ah, Kirk, my old friend...');
 		}
-
-		$repository = $em->getRepository(Article::class);
-		/** @var Article $article */
-		$article = $repository->findOneBy(['slug' => $slug]);
-		if (!$article) {
-            throw $this->createNotFoundException(sprintf('No article for slug "%s"', $slug));
-        }
 
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
@@ -25,8 +26,9 @@ class ArticleRepository extends ServiceEntityRepository
     
     public function findAllPublishedOrderedByNewest()
     {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.publishedAt IS NOT NULL')
+        #$qb = $this->createQueryBuilder('a');
+
+        return $this->addIsPublishedQueryBuilder()
             ->orderBy('a.publishedAt', 'DESC')
             ->getQuery()
             ->getResult()
@@ -45,4 +47,15 @@ class ArticleRepository extends ServiceEntityRepository
         ;
     }
     */
+
+    private function addIsPublishedQueryBuilder(QueryBuilder $qb = null)
+    {
+        return $this->getOrCreateQueryBuilder($qb)
+            ->andWhere('a.publishedAt IS NOT NULL');
+    }
+
+    private function getOrCreateQueryBuilder(QueryBuilder $qb = null)
+    {
+        return $qb ?: $this->createQueryBuilder('a');
+    }
 }
