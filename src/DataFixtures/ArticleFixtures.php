115d1d4862aec1c6b581b54cf973da9d778e39c0
unknown <saswata@moviloglobal.com>
2018-12-20 15:32:45 +0530

    4:17 When Migrations Fail

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -32,11 +32,13 @@ Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi 
 lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
 labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
 **turkey** shank eu pork belly meatball non cupim.
+
 Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
 laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
 capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
 picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
 occaecat lorem meatball prosciutto quis strip steak.
+
 Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
 mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
 strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
@@ -51,6 +53,8 @@ EOF
 	        $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
 	            ->setHeartCount($this->faker->numberBetween(5, 100))
 	            ->setImageFilename($this->faker->randomElement(self::$articleImages))
+	            ->setCreatedAt(new \DateTime())
+	            ->setUpdatedAt(new \DateTime())
 	        ;
 	    });
 
