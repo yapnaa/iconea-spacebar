e24330ae2c5a1a8a2f89b5ed5c0047badd08efa9
unknown <saswata@moviloglobal.com>
2018-12-21 00:57:07 +0530

    5:18 Saving a ManyToMany Relation + Joins

--- src/DataFixtures/ArticleFixture.php
+++ src/DataFixtures/ArticleFixture.php
@@ -3,9 +3,11 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use App\Entity\Tag;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ArticleFixture extends BaseFixture
+class ArticleFixture extends BaseFixture implements DependentFixtureInterface
 {
     private static $articleTitles = [
         'Why Asteroids Taste Like Bacon',
@@ -43,14 +45,25 @@ EOF
             $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
                 ->setHeartCount($this->faker->numberBetween(5, 100))
                 ->setImageFilename($this->faker->randomElement(self::$articleImages))
-                ->setCreatedAt(new \DateTime())
-                ->setUpdatedAt(new \DateTime())
+                #->setCreatedAt(new \DateTime())
+                #->setUpdatedAt(new \DateTime())
             ;
 
+            $tags = $this->getRandomReferences(Tag::class, $this->faker->numberBetween(0, 5));
+            foreach ($tags as $tag) {
+                $article->addTag($tag);
+            }
             #$article->addComment($comment1);
             #$article->addComment($comment2);
         });
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            TagFixture::class,
+        ];
     }
 }
