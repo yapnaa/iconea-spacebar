c37342ea25c988b398c302973977c9ffd991613c
unknown <saswata@moviloglobal.com>
2018-12-20 16:25:48 +0530

    5:2 Adding the ManyToOne Relation

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -28,6 +28,12 @@ class Comment
      */
     private $content;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Article", inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $article;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -53,6 +59,18 @@ class Comment
     public function setContent(string $content): self
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function getArticle(): ?Article
+    {
+        return $this->article;
+    }
+
+    public function setArticle(?Article $article): self
+    {
+        $this->article = $article;
 
         return $this;
     }
