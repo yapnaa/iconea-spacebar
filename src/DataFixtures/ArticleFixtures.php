925ad18729313bb4dbfdb4ef1049f4bb9fcd755e
unknown <saswata@moviloglobal.com>
2018-12-20 15:16:24 +0530

    4:16 Sluggable & other Wonderful Behaviors

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -20,13 +20,13 @@ class ArticleFixtures extends BaseFixture
         'Mike Ferengi',
         'Amy Oort',
     ];
-    
+
     public function loadData(ObjectManager $manager)
     {
 
     	$this->createMany(Article::class, 10, function(Article $article, $count) {
 	        $article->setTitle($this->faker->randomElement(self::$articleTitles))
-	            ->setSlug($this->faker->slug)
+	            #->setSlug($this->faker->slug)
 	            ->setContent(<<<EOF
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
 lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
