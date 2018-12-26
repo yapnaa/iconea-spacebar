9c2b904d07b4a45923c2d2e8f0e9a69db6e1063e
unknown <saswata@moviloglobal.com>
2018-12-27 02:41:43 +0530

    6:15 Deny Access in the Controller

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -3,10 +3,14 @@ namespace App\Controller;
 use App\Entity\Article;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @IsGranted("ROLE_ADMIN")
+ */
 class ArticleAdminController extends AbstractController
 {
 	/**
--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -4,17 +4,24 @@ namespace App\Controller;
 use App\Repository\CommentRepository;
 
 use Knp\Component\Pager\PaginatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @IsGranted("ROLE_ADMIN")
+ */
 class CommentAdminController extends AbstractController
 {
     /**
      * @Route("/admin/comment", name="comment_admin")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function index(CommentRepository $repository, Request $request, PaginatorInterface $paginator)
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
     	$q = $request->query->get('q');
     	#$comments = $repository->findBy([], ['createdAt' => 'DESC']);
     	#$comments = $repository->findAllWithSearch($q);
