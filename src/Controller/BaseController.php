1577910b6ab1f545010e56d75d286a7338767ffb
unknown <saswata@moviloglobal.com>
2018-12-27 13:08:21 +0530

    6:18 Fetch the User Object

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @method User|null getUser()
+ */
+abstract class BaseController extends AbstractController
+{
+    protected function getUser(): User
+    {
+        return parent::getUser();
+    }
+}
