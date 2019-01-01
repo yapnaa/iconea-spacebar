f2b45926ebba7693d9c5cb9f62cae8ddcee225b2
unknown <saswata@moviloglobal.com>
2019-01-01 17:36:26 +0530

    3:3 Success (Flash) Messages

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -32,7 +32,9 @@ class ArticleAdminController extends AbstractController
 
             $em->persist($article);
             $em->flush();
-            return $this->redirectToRoute('app_homepage');
+
+            $this->addFlash('success', 'Article Created! Knowledge is power!');
+            return $this->redirectToRoute('admin_article_list');
         }
 
         return $this->render('article_admin/new.html.twig', [
@@ -54,7 +56,7 @@ class ArticleAdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/article")
+     * @Route("/admin/article", name="admin_article_list")
      */
     public function list(ArticleRepository $articleRepo)
     {
